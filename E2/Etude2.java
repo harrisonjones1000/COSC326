21248679885f50589fd2830784f2ab26bf395a5d
hurol081 <hurol081@student.otago.ac.nz>
2025-03-05 12:34:40 +1300

    added token parser

--- E2/Etude2.java
+++ E2/Etude2.java
@@ -3,20 +3,27 @@
  * Notes: assume each text file has the same format throughout
  */
 
-package E2;
+// package E2;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Etude2 {
     public static void main(String[] args) {
-        try{
-            Scanner testScan = new Scanner(new File("E1/E1_tests.txt"));
-            while(testScan.hasNext()) System.out.println(testScan.nextLine());
+        ArrayList<String[]> tokenList = new ArrayList<>();
+        try {
+            Scanner testScan = new Scanner(new File("test1.txt"));
+            while (testScan.hasNext())
+                tokenList.add(testScan.nextLine().split("/"));
             testScan.close();
 
-        }catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("No file of that name found");
+        }
+        for (String[] tokens : tokenList) {
+            System.out.println(
+                    "First token: " + tokens[0] + "\nSecond token: " + tokens[1] + "\nThird token: " + tokens[2]);
         }
     }
 }
