ceaccd531533ce1d6dbb1412f6665770db4e21b0
hurol081 <hurol081@student.otago.ac.nz>
2025-03-05 14:06:27 +1300

    added prototype method for checking d/y/m

--- E2/Etude2.java
+++ E2/Etude2.java
@@ -1,3 +1,4 @@
+
 /**
  * Harrison Jones and Ollie Hurst
  * Notes: assume each text file has the same format throughout
@@ -28,12 +29,62 @@ public class Etude2 {
         }
     }
 
-    private boolean dmy(String[] tokens){
+    private boolean dmy(String[] tokens) {
         int d = Integer.parseInt(tokens[0]);
         int m = Integer.parseInt(tokens[1]);
         int y = Integer.parseInt(tokens[2]);
 
-        
+        return true;
+    }
+
+    private static boolean dym(String[] tokens) {
+
+        String d = tokens[0];
+        String y = tokens[1];
+        String m = tokens[2];
+
+        int dayInt;
+        int yearInt;
+        int monthInt;
+
+        try {
+            dayInt = Integer.parseInt(d);
+            yearInt = Integer.parseInt(y);
+            monthInt = Integer.parseInt(m);
+        } catch (Exception e) {
+            return false;
+        }
+
+        if (y.length() == 2) {
+            if (yearInt < 49) {
+                yearInt = yearInt + 2000;
+            } else if (yearInt >= 50) {
+                yearInt = yearInt + 1900;
+            }
+        }
+
+        boolean isLeapYear = false;
+        if (yearInt % 4 == 0 && !(yearInt % 100 == 0) && !(yearInt % 400 == 0)) {
+            isLeapYear = true;
+        }
+
+        if (monthInt < 1 || monthInt > 12) { // month out of range
+            return false;
+        }
+
+        // the max number of days by month (if not leap year)
+        int[] daysInMonths = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        int maxDays;
+
+        if (isLeapYear && monthInt == 2) {
+            maxDays = 29;
+        } else {
+            maxDays = daysInMonths[monthInt];
+        }
+
+        if (dayInt < 1 || dayInt > maxDays) {
+            return false;
+        }
 
         return true;
     }
