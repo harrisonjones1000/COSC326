fec4c6dcc8766d7636e10cab94cc56586b46f183
jonha617 <jonha617@student.otago.ac.nz>
2025-03-12 13:09:39 +1300

    E1 fix

--- E1/Etude1.java
+++ E1/Etude1.java
@@ -5,27 +5,11 @@ import java.util.Scanner;
 
 public class Etude1 {
     public static void main(String[] args) {
-        try{
-            if(args.length==0){
-                System.out.println("No test file provided");
-            }else{
-                Scanner testScan = new Scanner(new File(args[0]));
-                if(args.length==1){
-                    while(testScan.hasNext()) System.out.print(method(testScan.nextLine()));
-                }else if(args.length==2){
-                    try (FileWriter writer = new FileWriter(args[1], false)) {
-                        while(testScan.hasNext()) writer.write(method(testScan.nextLine()));
-                    } catch (IOException e) {}
-                      
-                }else{
-                    System.out.println("Invalid input");
-                }
-                testScan.close();
-            }  
-
-        }catch(FileNotFoundException e){
-            System.out.println("No file of that name found");
+        Scanner testScan = new Scanner(System.in);
+        while (testScan.hasNext()) {
+            System.out.print(method(testScan.nextLine()));
         }
+        testScan.close();
     }
 
     public static String method(String email){
