8193d0f75a7677ba8e8f1b7d574dc4fd9a38b140
jonha617 <jonha617@student.otago.ac.nz>
2025-03-12 13:15:16 +1300

    E9 I/O fix

--- E9/Arithmetic2.java
+++ E9/Arithmetic2.java
@@ -1,28 +1,14 @@
 //Harrison Jones
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Arithmetic2 {
     public static void main(String[] args) {
-        try{
-            Scanner testScan = new Scanner(new File(args[0]));
-            if(args.length==1){
-                while(testScan.hasNext()) System.out.print(e9(testScan.nextLine()));
-            }else if(args.length==2){
-                try (FileWriter writer = new FileWriter(args[1], false)) {
-                    while(testScan.hasNext()) writer.write(e9(testScan.nextLine()));
-                } catch (IOException e) {}
-                  
-            }
-
-            testScan.close();
-        }catch(FileNotFoundException e){
-            System.out.println("No file of that name found");
+        Scanner testScan = new Scanner(System.in);
+        while (testScan.hasNext()) {
+            System.out.print(e9(testScan.nextLine()));
         }
+        testScan.close();
     }
 
     public static String e9(String input){
@@ -79,8 +65,3 @@ public class Arithmetic2 {
     }    
 
 }
-
-
-
-
-
