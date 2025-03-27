e96c235df497e0776bcd550980e6f721db9d74b4
hurol081 <hurol081@student.otago.ac.nz>
2025-03-27 16:59:34 +1300

    improved circle search

--- etude-03/etude-03.py
+++ etude-03/etude-03.py
@@ -46,51 +46,19 @@ circle_coords = np.array(circle_coords)
 circle_rads = np.array(circle_rads)
 
 print(f"Solver took {time.time()-solver_st} seconds")
-
-
-norm_st = time.time()
-# distance from p0 to all circles, distance from p1 to all circles, ...
-norms = []
-for p in points:
-    norms.append(norm(circle_coords - p, axis=1))
-
-norms = np.array(norms)
-
-print(f"Norms took: {time.time()-norm_st} seconds")
-
 find_circle_st = time.time()
 
-circle_rads_tol = np.abs(circle_rads) + 0.0000000001
-circle_rads_tol_rep = np.tile(circle_rads_tol, len(points))
-circle_rads_tol_rep = np.reshape(circle_rads_tol_rep, (len(points), len(circle_coords)))
+circles = np.column_stack([circle_coords, circle_rads])
+sorted_circles = circles[circles[:, 2].argsort()]
 
+def get_smallest_circle(sorted_circles):
+    for i in range(len(sorted_circles)):
+        n_points = (norm(points - sorted_circles[i, 0:2], axis=1) <= sorted_circles[i, 2] + 1e-5).sum()
+        if n_points > 11:
+            return i
 
-# each row is whether a point is contained within the circle of that column
-
-# each column is associated with a circle, each row with a point
-# True if point is in circle (therefore row sum to get number of points by circle)
-point_in_circle = norms <= circle_rads_tol_rep
-counts = point_in_circle.sum(axis=0)
-contains_twelve = counts == 12
-i_circle_ans = circle_rads[contains_twelve].argmin()
+index = get_smallest_circle(sorted_circles)
 
 print(f"Finding circle took: {time.time()-find_circle_st} seconds")
-print(f"Time taken: {time.time()-start_time}")
-print(f"Circle: {circle_coords[contains_twelve][i_circle_ans]}, {circle_rads[contains_twelve][i_circle_ans]}")
-
-# print(norms)
-# print()
-# print(circle_rads_tol_rep)
-# print()
-# print(point_in_circle)
-# print()
-# print(counts)
-# print()
-# print(contains_twelve)
-# print()
-# print(circle_coords[contains_twelve])
-# print()
-# print(circle_rads[contains_twelve][i_circle_ans])
-# print()
-# print(i_circle_ans)
-# print(contains_twelve[i])
+print(f"Circle: {sorted_circles[index]}")
+print(f"Total time taken: {time.time()-start_time}")
