ea71212eefc609bc833e8a57a6a7da6a4f840a07
Harrison <harrisonbjones2003@gmail.com>
2025-04-21 19:42:36 +1200

    Fin

--- E7/CaseGen.java
+++ E7/CaseGen.java
@@ -1,67 +1,64 @@
-import java.util.Random;
-
 public class CaseGen {
     public static void main(String[] args){
-        String print;
-        Random rs = new Random();
 
-        String starter;
-        String end;
-
-        for(int s=0; s<3; s++){ // subject
+        for (int s = 0; s < 3; s++) { // subject
             String[] subjects;
         
-            if(s==0){
+            if (s == 0) {
                 subjects = new String[]{"I", "You", "He", "She"};
-            } else if(s==2){
-                subjects = new String[]{"We (2 incl)", "We (2 excl)", "They (2 excl)", "You two"};
+            } else if (s == 2) {
+                subjects = new String[]{"We (2 incl)", "We (2 excl)", "You (2 incl)", "You two", "They (2 excl)"};
             } else {
-                subjects = new String[]{"We (3 incl)", "We (3 excl)", "They (3 excl)", "You (3 excl)"};
+                subjects = new String[]{"We (3 incl)", "We (3 excl)", "You (3 incl)", "They (3 excl)"};
             }
-
-            for(int ss=0; ss<subjects.length; ss++){
-                starter = subjects[ss];
-
-                for(int o=0; o<4; o++){ //object
+        
+            for (int ss = 0; ss < subjects.length; ss++) {
+                String starter = subjects[ss];
+        
+                for (int o = 0; o < 4; o++) { // object
                     String[] objects;
-                    if(o==0){
+                    if (o == 0) {
                         objects = new String[]{""};
-                    }else if(o==1){
+                    } else if (o == 1) {
                         objects = new String[]{"me", "you", "him", "her"};
-                    }else if(o==2){
-                        objects = new String[]{"you two", "them"};
-                    }else{
-                        objects = new String[]{"us", "you", "them"};
+                    } else if (o == 2) {
+                        objects = new String[]{"us (2 incl)", "us (2 excl)", "you two", "you (2 incl)", "them (2 excl)"};
+                    } else {
+                        objects = new String[]{"us (3 incl)", "us (3 excl)", "you (3 incl)", "them (3 excl)"};
                     }
-                    
-                    for(int oo=0; oo<objects.length; oo++){
-                        end = objects[oo];
-
-                        for(int t=0; t<3; t++){//tense
-                            String[] verbs;
-
-                            //go, make, see, want, call, ask, read, learn
-                            if(t==0){//past
-                                verbs = new String[]{"went", "made", "wanted", "saw", "called", "asked", "read", "learnt"};
-                            }else if(t==1){ //present
-                                verbs = new String[]{"go", "make", "see", "want", "call", "ask", "read", "learn"};
-                            }else{ //future
-                                verbs = new String[]{"will go", "will make", "will see", "will want", "will call", "will ask", "will read", "will learn"};
+        
+                    for (int t = 0; t < 3; t++) { // tense
+                        String phraseStarter = starter;
+        
+                        if (t==1) {
+                            if (phraseStarter.equals("I")) {
+                                phraseStarter = "I am";
+                            } else if (phraseStarter.equals("He") || phraseStarter.equals("She")) {
+                                phraseStarter = "He is";
+                            } else {
+                                phraseStarter = phraseStarter + " are";
                             }
-
-                            for(int v=0; v<8; v++){
-                                if(starter.equals(end)){
-                                    System.out.println("err");
-                                }else{
-                                    //System.out.println(starter + " " + verbs[v] + " " + end + " : " + s + " " + ss + " " + o + " " + oo);
-                                    System.out.println(starter + " " + verbs[v] + " " + end);
-
-                                }
+                        }
+        
+                        String[] verbs;
+                        if(t == 0){ // past
+                            verbs = new String[]{"went", "made", "wanted", "saw", "called", "asked", "read", "learnt"};
+                        }else if (t == 1){ // present
+                            verbs = new String[]{"going", "making", "seeing", "wanting", "calling", "asking", "reading", "learning"};
+                        }else{ // future
+                            verbs = new String[]{"will go", "will make", "will see", "will want", "will call", "will ask", "will read", "will learn"};
+                        }
+        
+                        for(int oo = 0; oo < objects.length; oo++) {
+                            String end = objects[oo];
+        
+                            for(int v = 0; v < verbs.length; v++){
+                                System.out.println(phraseStarter + " " + verbs[v] + " " + end);
                             }
                         }
                     }
                 }
             }
-        }
+        }        
     }
 }
--- E7/E72.java
+++ E7/E72.java
@@ -14,70 +14,106 @@ public class E72 {
 
         String[] sentence;
         int num;
-        String object="";
+        String subject="";
         int pos = -1;
         
         String tense = "";
         String verb = "";
 
+        boolean start, end;
+
+        String object = "";
+
         Map<String, String> one = new HashMap<>();
-        one.put("I", "au");
+        one.put("I", "ahau");
         one.put("You", "koe");
         one.put("He", "ia");
         one.put("She", "ia");
 
+        one.put("me", "ahau");
+        one.put("you", "ia");
+        one.put("him", "ia");
+        one.put("her", "ia");
+
         Map<String, String> two = new HashMap<>();
         two.put("We (2 incl)", "tāua");
         two.put("We (2 excl)", "māua");
+        two.put("You (2 incl)", "kōrua"); 
         two.put("They (2 excl)", "rāua");
-        //two.put("You two", "kōrua"); 
+
+        two.put("us (2 incl)", "tāua"); 
+        two.put("us (2 excl)", "māua"); 
+        two.put("you (2 incl)", "kōrua"); 
+        two.put("you two", "kōrua"); 
+        two.put("them (2 excl)", "rāua"); 
 
         Map<String, String> three = new HashMap<>();
-        three.put("We (3 incl)", "tāua");
-        three.put("We (3 excl)", "māua");
-        three.put("They (3 excl)", "rāua");
-        three.put("You (3 excl)", "kōrua"); 
+        three.put("We (3 incl)", "tātou");
+        three.put("We (3 excl)", "mātou");
+        three.put("You (3 incl)", "koutou"); 
+        three.put("They (3 excl)", "rātou");
+
+        three.put("us (3 incl)", "tātou"); 
+        three.put("us (3 excl)", "mātou"); 
+        three.put("you", "koutou"); 
+        three.put("them (3 excl)", "rātou"); 
+
+        Map<String, String> future = new HashMap<>();
+        future.put("go", "haere ");
+        future.put("make", "hanga ");
+        future.put("see", "kite ");
+        future.put("want", "hiahia ");
+        future.put("call", "karanga ");
+        future.put("ask", "pātai ");
+        future.put("read", "pānui ");
+        future.put("learn", "ako ");
 
         Map<String, String> present = new HashMap<>();
-        present.put("go", "haere");
-        present.put("make", "hanga");
-        present.put("see", "kite");
-        present.put("want", "hiahia");
-        present.put("call", "karanga");
-        present.put("ask", "pātai");
-        present.put("read", "pānui");
-        present.put("learn", "ako");
+        present.put("going", "haere ");
+        present.put("making", "hanga ");
+        present.put("seeing", "kite ");
+        present.put("wanting", "hiahia ");
+        present.put("calling", "karanga ");
+        present.put("asking", "pātai ");
+        present.put("reading", "pānui ");
+        present.put("learning", "ako ");
 
         Map<String, String> past = new HashMap<>();
-        past.put("went", "haere");
-        past.put("made", "hanga");
-        past.put("saw", "kite");
-        past.put("wanted", "hiahia");
-        past.put("called", "karanga");
-        past.put("asked", "pātai");
-        past.put("read", "pānui");
-        past.put("learnt", "ako");
+        past.put("went", "haere ");
+        past.put("made", "hanga ");
+        past.put("saw", "kite ");
+        past.put("wanted", "hiahia ");
+        past.put("called", "karanga ");
+        past.put("asked", "pātai ");
+        past.put("read", "pānui ");
+        past.put("learnt", "ako ");
 
-        while (testScan.hasNext()){
+        while(testScan.hasNext()){
+            start=false;
+            end=false;
+
             sentence = testScan.nextLine().split(" ");
+
+            if (sentence[0].equals("I") | sentence[0].equals("We")) start = true;
+
             try{
                 num = Integer.parseInt(sentence[1].substring(1));
                 if(num<=1){
                     System.out.println("INVALID1");
                     continue;
                 }else if(num==2){//Two people
-                    object = two.get(sentence[0] + " " + sentence[1] + " " + sentence[2]);
+                    subject = two.get(sentence[0] + " " + sentence[1] + " " + sentence[2]);
                 }else if(num>2){//Three people
                     sentence[1] = "(3";
-                    object = three.get(sentence[0] + " " + sentence[1] + " " + sentence[2]);
+                    subject = three.get(sentence[0] + " " + sentence[1] + " " + sentence[2]);
                 }
                 pos=3;
             }catch(NumberFormatException e){ //One person
                 if("You two".equals(sentence[0] + " " + sentence[1])){
-                    object = "kōrua";
+                    subject = "kōrua";
                     pos = 2;
                 }else{
-                    object = one.get(sentence[0]);
+                    subject = one.get(sentence[0]);
                     pos=1;
                 }
             }catch(IndexOutOfBoundsException e){
@@ -85,7 +121,7 @@ public class E72 {
                 continue;
             }
 
-            if(object==null){
+            if(subject==null){
                 System.out.println("INVALID3");
                 continue;
             }
@@ -93,23 +129,82 @@ public class E72 {
             try{
                 if(sentence[pos].equals("will")){
                     tense = "Ka ";
-                    verb = present.get(sentence[pos+1]);
-                }else if(present.get(sentence[pos])!=null){
-                    tense = "Kei te ";
-                    verb = present.get(sentence[pos]);
+                    verb = future.get(sentence[pos+1]);
+                    pos+=2;
                 }else if(past.get(sentence[pos])!=null){
                     tense = "I ";
                     verb = past.get(sentence[pos]);
+                    pos++;
+                }else if(pos==1){
+                    if((sentence[pos-1].equals("I") & sentence[pos].equals("am")) | (sentence[pos-1].equals("You") & sentence[pos].equals("are")) | (((sentence[pos-1].equals("She")) | (sentence[pos-1].equals("He"))) & sentence[pos].equals("is"))){
+                        tense = "Kei te ";
+                        verb = present.get(sentence[pos+1]);
+                        pos+=2;
+                    }else{
+                        System.out.println("INVALID4");
+                        continue;
+                    }
+                }else if(sentence[pos].equals("are")){
+                    tense = "Kei te ";
+                    verb = present.get(sentence[pos+1]);
+                    pos+=2;
+                }else{
+                    System.out.println("INVALID5");
+                    continue;
                 }
             }catch(IndexOutOfBoundsException e){
-                System.out.println("INVALID4");
+                System.out.println("INVALID6");
                 continue;
             }
+   
+            try{
+                object = sentence[pos];
+                if((object.equals("me") | object.equals("us")) & start){
+                    System.out.println("INVALID7"); //subject and object shouldnt be the same
+                    continue;
+                }else if(verb.equals("haere ") | verb.equals("pānui ") | verb.equals("ako ")){
+                    System.out.println("INVALID8"); //objects for these verbs don't make sense
+                    continue;
+                }
+            }catch(IndexOutOfBoundsException e){
+                System.out.println(tense + verb + subject);
+                continue;
+            } 
             
-            System.out.println(tense + verb);
-            
-        }
+            if(!object.isEmpty()){
+                if(!Character.isLowerCase(object.charAt(0))){
+                    System.out.println("INVALID9"); //allows us to keep using same HashMaps
+                    continue; 
+                }
+            }
 
+            try{
+                if(sentence.length-pos == 1){
+                    object = one.get(object);
+                }else if(sentence.length-pos == 3){
+                    num = Integer.parseInt(sentence[pos+1].substring(1));
+                    if(num<=1){
+                        System.out.println("INVALID11");
+                        continue;
+                    }else if(num==2){//Two people
+                        object = two.get(object + " " + sentence[pos+1] + " " + sentence[pos+2]);
+
+                    }else if(num>2){//Three people
+                        sentence[pos+1] = "(3";
+                        object = three.get(object + " " + sentence[pos+1] + " " + sentence[pos+2]);
+
+                    }
+                }else if((object + " " + sentence[pos+1]).equals("you two")){
+                    object = "kōrua";
+                }
+            }catch(NumberFormatException e){
+                System.out.println("INVALID12");
+                continue; 
+            }
+
+            System.out.println(tense + verb + subject + " " + object);
+
+        }
         testScan.close();
     }
 }
