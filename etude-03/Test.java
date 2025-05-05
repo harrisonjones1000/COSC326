dc2bbe123170df85ce4f7e93da14da677215b269
magicalmanjack <magicalmanjack@gmail.com>
2025-05-06 11:46:28 +1200

    added IBMFloat32 structure.

--- etude-03/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        if(args[0].matches("-?[0-9]*\\.[0-9]*")) {
-            System.out.println("matches");
-        } else {
-            System.out.println("does not match");
-        }
-    }
-}
---
+++ etude-12/IBMFloat32.java
@@ -0,0 +1,50 @@
+public class IBMFloat32 {
+
+    /*
+     * S   EEEEEEE   FFFF FFFF FFFF FFFF FFFF FFFF
+     * 31  30 - 24   23 ------------------------ 0 
+     */
+
+    private byte[] data;
+
+    public IBMFloat32() {
+        data = new byte[4];
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public byte getExp() {
+        byte mask = (byte)0x7F;
+        return (byte)(data[0] & mask);
+    }
+
+    public void setExp(byte exp) {
+        data[0] = (byte)(getSign() | exp);
+    }
+
+    public byte getSign() {
+        byte mask = (byte)0x80;
+        return (byte)(data[0] & mask);
+    }
+
+    public void setSign(byte sign) {
+        data[0] = (byte)(sign | getExp());
+    }
+
+    public byte[] getFraction() {
+        return new byte[] {data[2], data[3]};
+    }
+
+    public void setFraction(byte[] fraction) {
+        data[2] = fraction[0];
+        data[3] = fraction[1];
+    }
+
+
+}
