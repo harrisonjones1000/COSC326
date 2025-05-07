7736041403c5a9e47f9fbf015d53506c23e5bfc6
hurol081 <hurol081@student.otago.ac.nz>
2025-05-08 10:25:28 +1200

    added weird ideas to git

--- etude-06/E6.py
+++ etude-06/E6.py
@@ -6,6 +6,7 @@ import numpy as np
 from scipy.io import wavfile
 from scipy.fft import rfft
 import warnings
+import sys
 
 warnings.filterwarnings("ignore")
 
@@ -34,10 +35,34 @@ def print_notes(song_name):
     w = round(sample_rate/2) # width, 500ms
     n_steps = round(len(data)/(sample_rate/10)) # number of steps, 10 per second
     i = round((len(data)-w)/(n_steps-1)) # amount to move window by
+    resolution_of_fft = sample_rate/w
+    print(f"{resolution_of_fft=} Hz")
 
     notes = []
     # male_notes = 0
     # female_notes = 0
+
+    # window = hamming(w, sym=True)
+    # SFT = ShortTimeFFT(window, i, sample_rate)
+    # result = SFT.stft(data)
+    # print(f"{n_steps=}")
+    # print(f"{sample_rate=}")
+    # print(f"{w=}")
+    # print(f"{len(data)=}")
+    # print(result.shape)
+    # plt.plot(result[:, 19])
+    # plt.show()
+    # for i in range(result.shape[1]):
+    #     mode = np.argmax(result[:, i])
+    #     key_n = get_key_number(mode)
+    #     note = get_note(key_n)
+
+    #     # print(f"{mode=}")
+    #     # print(f"{key_n=}")
+    #     print(f"{note=}")
+    # exit()
+
+
     for n in range(n_steps):
         # Take sliding windows of length w
         # Sliding by i each iteration, 100 times
@@ -45,7 +70,6 @@ def print_notes(song_name):
         transformed_slice = rfft(data[n*i:n*i+w])
         transformed_slice = abs(transformed_slice)[:801]
         mode = np.argmax(transformed_slice)
-
         key_n = get_key_number(mode)
         # if key_n >= 20 and key_n <= 58:
         #     male_notes += 1
@@ -53,8 +77,51 @@ def print_notes(song_name):
         #     female_notes += 1
         note = get_note(key_n)
         notes.append(note)
+        cepstrum = np.abs(irfft(np.log(np.abs(rfft(data[n*i:n*i+w])))))#[60:801]
+        # cepstrum = np.reshape(cepstrum, (-1, 1))
+        ix = np.arange(len(cepstrum))
+        # ix = np.reshape(ix, (-1, 1))
+        # print(ix.shape)
+        cepstrum = np.vstack([cepstrum, ix]).T
 
-    # print(f"{'-'.join(notes)}")
+        l_bound = sample_rate/800
+        u_bound = sample_rate/60
+
+        # print(f"{l_bound=}")
+        # print(f"{u_bound=}")
+
+        # print(f"{cepstrum[:, 1]=}")
+        # print(f"{u_bound=}")
+
+        # print(f"{len(cepstrum)=}")
+
+        condition = np.all([cepstrum[:, 1] > l_bound, cepstrum[:, 1] < u_bound], axis=0)
+        cepstrum = cepstrum[condition, :]
+        # cepstrum = cepstrum[cepstrum[:, 1] < u_bound]
+
+        quefrency = cepstrum[cepstrum[:, 0].argmax(), 1]
+        frequency = sample_rate/quefrency
+        key_n = get_key_number(frequency)
+        note2 = get_note(key_n)
+        print(f"{note=}")
+        print(f"{note2=}")
+
+        # print(f"{quefrency=}")
+        # print(f"{sample_rate/quefrency=}")
+
+
+
+
+
+        # print(cepstrum)
+        # print(f"{cepstrum.shape=}")
+
+        # print(ix)
+        # plt.scatter(y=cepstrum[:, 0], x=cepstrum[:, 1])
+        # plt.show()
+        # exit()
+
+    print(f"{'-'.join(notes)}")
 
     filtered_notes = [None]
 
@@ -63,7 +130,7 @@ def print_notes(song_name):
             filtered_notes.append(a)
     filtered_notes.pop(0)
 
-    print(f"Song: {song_name}")
+    # print(f"Song: {song_name}")
     print("-".join(filtered_notes))
 
     # if male_notes > female_notes:
@@ -71,8 +138,11 @@ def print_notes(song_name):
     # else:
     #     print("Female voice")
 
-print_notes('song1.wav')
-print()
-print_notes('song2.wav')
+path_to_song = sys.argv[1]
+
+# print(f"{path_to_song=}")
+print_notes(path_to_song)
+# print()
+# print_notes('song2.wav')
 
 
